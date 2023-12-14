b78aab28f92359e7a564c65f5659f13d2e3715a6
Enni Kanerva Rajala <enni.rajala@gmail.com>
2023-12-14 18:43:25 +0200

    visuaalinen ilme luotu

--- Laskin/Program.cs
+++ Laskin/Program.cs
@@ -11,7 +11,7 @@ namespace Laskin
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new Laskin());
         }
     }
 }
